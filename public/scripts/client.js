ef75e32769a84b5aa6ea41471f896dc849044246
Fransiskus <aldzaidea@gmail.com>
2022-02-02 22:34:24 +0000

    give logic for empty tweet or char exceed the max char

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,40 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetsData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac",
-    },
-    "content": {
-      "text":
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    "created_at": 1643552739858,
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd",
-    },
-    "content": {
-      "text": "Je pense , donc je suis",
-    },
-    "created_at": 1643639139858,
-  },
-];
+// const tweetsData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac",
+//     },
+//     "content": {
+//       "text":
+//         "If I have seen further it is by standing on the shoulders of giants",
+//     },
+//     "created_at": 1643552739858,
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd",
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis",
+//     },
+//     "created_at": 1643639139858,
+//   },
+// ];
 
+//document ready so any javascript will be executed after all html file loaded
 $(document).ready(function () {
+  //function that will append new tweet to the last child of element
   const renderTweets = function (tweets) {
+    //loop through the tweet data and append to element container
     for (const key in tweets) {
       let $tweet = createTweetElement(tweets[key]);
-      $("#tweets-cointainer").append($tweet);
+      $("#tweets-container").append($tweet);
     }
   };
 
+  //create element of the tweet
   const createTweetElement = (tweet) => {
     let $tweet = `
   <article>
@@ -67,13 +71,32 @@ $(document).ready(function () {
   `;
     return $tweet;
   };
-  renderTweets(tweetsData);
 
+  //send get request from tweets path and render it
+  const loadTweets = () => {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      type: "GET",
+      success: function (data) {
+        renderTweets(data);
+      },
+      error: function (err) {
+        return err;
+      },
+    });
+  };
+  loadTweets();
+
+  //post new tweet with submit request
   $("#submit-tweet").submit(function (event) {
     event.preventDefault();
 
+    //condition check if tweet is empty or more than max char
     const data = $(this).serialize();
-
+    if (data !== "" || data !== null || data.length > 140) {
+      return alert("Cannot Submit tweet");
+    }
+    //if condtion is true
     $.ajax({
       url: "http://localhost:8080/tweets",
       type: "POST",
@@ -82,7 +105,7 @@ $(document).ready(function () {
         console.log(data);
       },
       error: function (err) {
-        console.log(err);
+        return err;
       },
     });
   });
